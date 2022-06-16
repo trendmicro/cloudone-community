2b558d754331bfc5c39ca3ff9aa99ffff9188713
Raphael Bottino <raphabot@gmail.com>
2022-06-16 08:03:16 -0500

    Adding cloud-one-integrations-security-hub-deployment.

---
+++ Integration/Deployment/cdk-cloudformation-deploy/test/security-hub.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SecurityHub from '../lib/security-hub-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/security-hub-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SecurityHub.SecurityHubStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
