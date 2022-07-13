204cd2205b3fb70a85c746e5c0071a0edb04b924
George Davis <george_davis@trendmicro.com>
2022-07-13 18:53:06 -0400

    Changed credentials library, commented an unused function, fixed list_locations SDK API request but staying with CLI approach until locations_metadata is also included in API response

--- File-Storage-Security/Deployment/azure-python-deploy-to-all-existing-s3/deployToAllExistingStorageAccounts/deploymentHandler/locations.py
+++ File-Storage-Security/Deployment/azure-python-deploy-to-all-existing-s3/deployToAllExistingStorageAccounts/deploymentHandler/locations.py
@@ -1,6 +1,9 @@
 from azure.mgmt.subscription import SubscriptionClient
 from azure.identity import DefaultAzureCredential
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.identity import ClientSecretCredential
 
+import osq
 import utils
 import random
 
@@ -22,20 +25,20 @@ def get_azure_recommended_location_by_geography_group(az_geography_group, az_geo
             return az_geography_groups_list[az_geography_groups_list.index(az_geography_group)][random.randint(0, len(az_geography_groups_list[az_geography_groups_list.index(az_geography_group)])-1)]["name"]
 
 # # TODO: Deprecate this function if not in use
-def get_azure_recommended_locations_list_by_geography_groups():
+# def get_azure_recommended_locations_list_by_geography_groups():
 
-    az_locations_list = utils.azure_cli_run_command('account list-locations')
+#     az_locations_list = utils.azure_cli_run_command('account list-locations')
 
-    unique_geography_groups_list = []
+#     unique_geography_groups_list = []
 
-    for az_location in az_locations_list:
-        if az_location["metadata"]["geographyGroup"] and az_location["metadata"]["geographyGroup"] not in unique_geography_groups_list:
-            unique_geography_groups_list.append(az_location["metadata"]["geographyGroup"])
+#     for az_location in az_locations_list:
+#         if az_location["metadata"]["geographyGroup"] and az_location["metadata"]["geographyGroup"] not in unique_geography_groups_list:
+#             unique_geography_groups_list.append(az_location["metadata"]["geographyGroup"])
 
-    for az_geography_group_item in unique_geography_groups_list:
-        for az_location in az_locations_list:
-            if az_location["metadata"]["geographyGroup"] == az_geography_group_item and az_location["metadata"]["regionCategory"] == "Recommended":
-                print(az_location["metadata"]["geographyGroup"], '-', az_location["metadata"]["regionCategory"])
+#     for az_geography_group_item in unique_geography_groups_list:
+#         for az_location in az_locations_list:
+#             if az_location["metadata"]["geographyGroup"] == az_geography_group_item and az_location["metadata"]["regionCategory"] == "Recommended":
+#                 print(az_location["metadata"]["geographyGroup"], '-', az_location["metadata"]["regionCategory"])
 
 # get_azure_supported_locations - Lists all supported locations for Azure in the current subscription.
 def get_azure_supported_locations():
@@ -57,37 +60,28 @@ def get_azure_supported_locations():
 
     return az_supported_locations_obj_by_geography_groups
 
-
-# get_azure_supported_locations - Lists all supported locations for Azure in the current subscription.
+# TODO: get_azure_supported_locations_sdk - Lists all supported locations for Azure in the current subscription via Azure SDK.
 def get_azure_supported_locations_sdk():
 
-    # self.credentials =  ServicePrincipalCredentials(
-    #                         client_id=client_id,
-    #                         secret=access_secret_key,
-    #                         tenant=tenant)
-
-    credentials = DefaultAzureCredential(exclude_environment_credential=False)
-    subscription_client = SubscriptionClient(credentials)
+    # credentials =  ServicePrincipalCredentials(
+    #     client_id=os.environ['AZURE_CLIENT_ID'],
+    #     secret=os.environ['AZURE_CLIENT_SECRET'],
+    #     tenant=os.environ['AZURE_TENANT_ID']
+    # )
+    credentials =  ClientSecretCredential(
+        client_id=os.environ['AZURE_CLIENT_ID'],
+        client_secret=os.environ['AZURE_CLIENT_SECRET'],
+        tenant_id=os.environ['AZURE_TENANT_ID']       
+    )
+    # credentials = DefaultAzureCredential(exclude_environment_credential=False)
+    subscription_client = SubscriptionClient(credentials, api_version='2021-01-01')
 
     subscription_id = utils.get_subscription_id()
 
-    az_locations_list = subscription_client.subscriptions.list_locations(subscription_id)
+    az_locations_iter = subscription_client.subscriptions.list_locations(subscription_id)    
     
-    for location in az_locations_list:
-        print(location.name)
+    az_locations_list = []
+    for location in az_locations_iter:
+        az_locations_list.append(location.name)
 
-    az_supported_locations_obj_by_geography_groups = {}
-
-    for az_location in az_locations_list:
-        if az_location["metadata"]["geographyGroup"] and az_location["metadata"]["geographyGroup"] not in az_supported_locations_obj_by_geography_groups.keys():
-            az_supported_locations_obj_by_geography_groups.update({az_location["metadata"]["geographyGroup"]: []})
-        elif not az_location["metadata"]["geographyGroup"]:
-            az_supported_locations_obj_by_geography_groups.update({"Logical": []})
-
-    for az_location in az_locations_list:
-        if az_location["metadata"]["regionType"] == "Physical":
-            az_supported_locations_obj_by_geography_groups[az_location["metadata"]["geographyGroup"]].append(az_location)
-        else:
-            az_supported_locations_obj_by_geography_groups["Logical"].append(az_location)
-
-    return az_supported_locations_obj_by_geography_groups
+    return az_locations_list
