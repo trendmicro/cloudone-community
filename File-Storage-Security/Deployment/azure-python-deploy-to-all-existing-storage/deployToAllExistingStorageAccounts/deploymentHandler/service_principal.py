21fbce21e1713d168e188f3bfc5bdd971a2cae31
George Davis <george_davis@trendmicro.com>
2022-07-19 15:59:35 -0400

    Adding the missing --all flag to the Azure CLI command, adding a getID method to improve code re-use

--- File-Storage-Security/Deployment/azure-python-deploy-to-all-existing-storage/deployToAllExistingStorageAccounts/deploymentHandler/service_principal.py
+++ File-Storage-Security/Deployment/azure-python-deploy-to-all-existing-storage/deployToAllExistingStorageAccounts/deploymentHandler/service_principal.py
@@ -2,9 +2,18 @@ import utils
 import os
 
 def query_service_principal(appId):
-    sp_list = utils.azure_cli_run_command('ad sp list')
+    sp_list = utils.azure_cli_run_command('ad sp list --all')
     for sp_item in sp_list:
         if sp_item["appId"] == appId:
-            print(str(sp_item["id"]))
+            # print(str(sp_item["id"]))
             return sp_item["id"]
     return None
+
+def get_service_principal_id(app_id):
+    service_principal_id = query_service_principal(app_id)
+
+    if not service_principal_id:
+        service_principal_id = utils.azure_cli_run_command('ad sp create --id ' + app_id)                
+    # rbac.createServicePrincipal()
+
+    return service_principal_id
