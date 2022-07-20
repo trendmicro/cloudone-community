84f3adef1e203334eaa374925b54786ff0d9ff00
George Davis <george_davis@trendmicro.com>
2022-07-19 23:30:54 -0400

    Fixing names and clarity issues, passing scanner stack uuid to storage stack for deployment and cloudone association

--- File-Storage-Security/Deployment/azure-python-deploy-to-all-existing-storage/deployToAllExistingStorageAccounts/deploymentHandler/deployment_single.py
+++ File-Storage-Security/Deployment/azure-python-deploy-to-all-existing-storage/deployToAllExistingStorageAccounts/deploymentHandler/deployment_single.py
@@ -2,14 +2,13 @@ import deployments
 import cloudone_fss_api
 import utils
 
-def deploy_single(subscription_id, azure_supported_locations_obj_by_geography_groups_dict, fss_supported_regions_list, deploy_storage_stack_list):
+def deploy_single(subscription_id, azure_supported_locations_obj_by_geography_groups_dict, fss_supported_regions_list, azure_storage_account_list):
 
     # Get All Scanner Stacks in this Azure Subscription
-    # scanner_stacks_list = cloudone_fss_api.filter_stacks_by_subscription_id(subscription_id, cloudone_fss_api.get_scanner_stacks())    
-    scanner_stacks_list = cloudone_fss_api.get_scanner_stacks()    
+    scanner_stacks_list = cloudone_fss_api.filter_stacks_by_subscription_id(subscription_id, cloudone_fss_api.get_scanner_stacks()) 
 
     temp_storage_account_dict = {}
-    for storage_account in deploy_storage_stack_list:
+    for storage_account in azure_storage_account_list:
 
         if storage_account["location"] not in temp_storage_account_dict:
             temp_storage_account_dict.update({storage_account["location"]: 1})
@@ -19,7 +18,7 @@ def deploy_single(subscription_id, azure_supported_locations_obj_by_geography_gr
     values_list = list(temp_storage_account_dict.values())
     max_storage_account_count = max(values_list)
 
-    scanner_stack_identity_principal_id = scanner_stack_queue_namespace = None
+    cloudone_scanner_stack_id = scanner_stack_identity_principal_id = scanner_stack_queue_namespace = None
 
     # If no Scanner Stack(s) exist in this Azure subscription
     if not len(scanner_stacks_list["stacks"]):
@@ -29,23 +28,25 @@ def deploy_single(subscription_id, azure_supported_locations_obj_by_geography_gr
         
         if scanner_stack_deployment_outputs:            
 
+            cloudone_scanner_stack_id = scanner_stack_deployment_outputs["cloudOneScannerStackId"]
             scanner_stack_identity_principal_id = scanner_stack_deployment_outputs["scannerIdentityPrincipalID"]["value"]
             scanner_stack_queue_namespace = scanner_stack_deployment_outputs["scannerQueueNamespace"]["value"]
 
         else:
             raise Exception("Deployment Failed. The deployment did not create any output(s). Check deployment status for more details on how to troubleshoot this issue.")
 
-    # TODO: Fetch Scanner Stack Identity Principal ID from the existing Scanner Stack Deployment Template Outputs
     else:        
-
+        # TODO: Choose the Scanner Stack in this subscription with the lowest number of storage stacks
+        cloudone_scanner_stack_id = scanner_stacks_list["stacks"][0]["stackID"]
         scanner_stack_identity_principal_id = scanner_stacks_list["stacks"][0]["details"]["scannerIdentityPrincipalID"]
         scanner_stack_queue_namespace = scanner_stacks_list["stacks"][0]["details"]["scannerQueueNamespace"]
 
-    if scanner_stack_identity_principal_id and scanner_stack_queue_namespace:
+    if cloudone_scanner_stack_id and scanner_stack_identity_principal_id and scanner_stack_queue_namespace:
 
+        print("cloudone_scanner_stack_id - " + str(cloudone_scanner_stack_id))
         print("scanner_stack_identity_principal_id - " + str(scanner_stack_identity_principal_id))
         print("scanner_stack_queue_namespace - " + str(scanner_stack_queue_namespace))
 
-        for storage_account in deploy_storage_stack_list:        
+        for storage_account in azure_storage_account_list:        
 
-            deployments.deploy_fss_storage_stack(subscription_id, storage_account, scanner_stack_identity_principal_id, scanner_stack_queue_namespace)
+            deployments.deploy_fss_storage_stack(subscription_id, storage_account, cloudone_scanner_stack_id, scanner_stack_identity_principal_id, scanner_stack_queue_namespace)
