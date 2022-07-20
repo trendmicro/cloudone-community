df2ffc2116272a33d76b7fb8d95dcafeeedceefe
George Davis <george_davis@trendmicro.com>
2022-07-20 03:15:47 -0400

    Removing print statements, renaming variable names

--- File-Storage-Security/Deployment/azure-python-deploy-to-all-existing-storage/deployToAllExistingStorageAccounts/deploymentHandler/locations.py
+++ File-Storage-Security/Deployment/azure-python-deploy-to-all-existing-storage/deployToAllExistingStorageAccounts/deploymentHandler/locations.py
@@ -8,57 +8,69 @@ import utils
 import random
 
 # get_azure_recommended_location_by_geography_group - Pick one Azure recommended location in the geography location. 
-def get_azure_recommended_location_by_geography_group(az_geography_group, az_geography_groups_list):
-    for az_geography_group_item in az_geography_groups_list:
-        if az_geography_group == az_geography_group_item:
+def get_azure_recommended_location_by_geography_group(azure_geography_group, azure_geography_groups_dict, fss_supported_regions_list):
+    for azure_geography_group_item in azure_geography_groups_dict:
+        if azure_geography_group == azure_geography_group_item:
 
-            print("Print List: " + str(az_geography_groups_list))
+            # print("Print Dict: " + str(azure_geography_groups_dict))
 
-            print("Index: " + str(az_geography_groups_list.index(az_geography_group)))
-            print("Value: " + str(az_geography_groups_list[az_geography_groups_list.index(az_geography_group)]))
-            print("Value 2: " + str(az_geography_groups_list[az_geography_groups_list.index(az_geography_group)]))
-            print("Length: " + str(len(az_geography_groups_list[az_geography_groups_list[az_geography_groups_list.index(az_geography_group)]])))
-            print("Random Index: " + str(random.randint(0, len(az_geography_groups_list[az_geography_groups_list.index(az_geography_group)])-1)))
-            print("Random Index Value: " + str(az_geography_groups_list[az_geography_groups_list.index(az_geography_group)][random.randint(0, len(az_geography_groups_list[az_geography_groups_list.index(az_geography_group)])-1)]))
-            print("Random Index Value Name: " + str(print("Random Index Value: " + str(az_geography_groups_list[az_geography_groups_list.index(az_geography_group)][random.randint(0, len(az_geography_groups_list[az_geography_groups_list.index(az_geography_group)])-1)]))))
+            temp_azure_geography_group_list = []
+            for azure_location in azure_geography_groups_dict[azure_geography_group]:
 
-            return az_geography_groups_list[az_geography_groups_list.index(az_geography_group)][random.randint(0, len(az_geography_groups_list[az_geography_groups_list.index(az_geography_group)])-1)]["name"]
+                if azure_location["metadata"]["regionCategory"] and azure_location["metadata"]["regionCategory"] == "Recommended" and azure_location["name"] in fss_supported_regions_list:
+
+                    temp_azure_geography_group_list.append(azure_location)
+
+            # print("Value: " + str(temp_azure_geography_group_list))
+            # print("Length: " + str(len(temp_azure_geography_group_list)))
+            # print("Random Location: " + str(random.randint(0, len(temp_azure_geography_group_list)-1)))
+            # print("Random Location Name: " + str(temp_azure_geography_group_list[random.randint(0, len(temp_azure_geography_group_list)-1)]["name"]))
+
+            return temp_azure_geography_group_list[random.randint(0, len(temp_azure_geography_group_list)-1)]["name"]    
 
 # # TODO: Deprecate this function if not in use
 # def get_azure_recommended_locations_list_by_geography_groups():
 
-#     az_locations_list = utils.azure_cli_run_command('account list-locations')
+#     azure_locations_list = utils.azure_cli_run_command('account list-locations')
 
 #     unique_geography_groups_list = []
 
-#     for az_location in az_locations_list:
-#         if az_location["metadata"]["geographyGroup"] and az_location["metadata"]["geographyGroup"] not in unique_geography_groups_list:
-#             unique_geography_groups_list.append(az_location["metadata"]["geographyGroup"])
+#     for azure_location in azure_locations_list:
+#         if azure_location["metadata"]["geographyGroup"] and azure_location["metadata"]["geographyGroup"] not in unique_geography_groups_list:
+#             unique_geography_groups_list.append(azure_location["metadata"]["geographyGroup"])
 
-#     for az_geography_group_item in unique_geography_groups_list:
-#         for az_location in az_locations_list:
-#             if az_location["metadata"]["geographyGroup"] == az_geography_group_item and az_location["metadata"]["regionCategory"] == "Recommended":
-#                 print(az_location["metadata"]["geographyGroup"], '-', az_location["metadata"]["regionCategory"])
+#     for azure_geography_group_item in unique_geography_groups_list:
+#         for azure_location in azure_locations_list:
+#             if azure_location["metadata"]["geographyGroup"] == azure_geography_group_item and azure_location["metadata"]["regionCategory"] == "Recommended":
+#                 print(azure_location["metadata"]["geographyGroup"], '-', azure_location["metadata"]["regionCategory"])
 
 # get_azure_supported_locations - Lists all supported locations for Azure in the current subscription.
 def get_azure_supported_locations():
-    az_locations_list = utils.azure_cli_run_command('account list-locations')
+    azure_locations_list = utils.azure_cli_run_command('account list-locations')
 
-    az_supported_locations_obj_by_geography_groups = {}
+    azure_supported_locations_obj_by_geography_groups = {}
 
-    for az_location in az_locations_list:
-        if az_location["metadata"]["geographyGroup"] and utils.trim_spaces(az_location["metadata"]["geographyGroup"]) not in az_supported_locations_obj_by_geography_groups.keys():
-            az_supported_locations_obj_by_geography_groups.update({utils.trim_spaces(az_location["metadata"]["geographyGroup"]): []})
-        elif not az_location["metadata"]["geographyGroup"]:
-            az_supported_locations_obj_by_geography_groups.update({"logical": []})
+    for azure_location in azure_locations_list:
+        if azure_location["metadata"]["geographyGroup"] and utils.trim_spaces(azure_location["metadata"]["geographyGroup"]) not in azure_supported_locations_obj_by_geography_groups.keys():
+            azure_supported_locations_obj_by_geography_groups.update({utils.trim_spaces(azure_location["metadata"]["geographyGroup"]): []})
+        elif not azure_location["metadata"]["geographyGroup"]:
+            azure_supported_locations_obj_by_geography_groups.update({"logical": []})
 
-    for az_location in az_locations_list:
-        if az_location["metadata"]["regionType"] == "Physical":
-            az_supported_locations_obj_by_geography_groups[utils.trim_spaces(az_location["metadata"]["geographyGroup"])].append(az_location)
+    for azure_location in azure_locations_list:
+        if azure_location["metadata"]["regionType"] == "Physical":
+            azure_supported_locations_obj_by_geography_groups[utils.trim_spaces(azure_location["metadata"]["geographyGroup"])].append(azure_location)
         else:
-            az_supported_locations_obj_by_geography_groups["logical"].append(az_location)
+            azure_supported_locations_obj_by_geography_groups["logical"].append(azure_location)
 
-    return az_supported_locations_obj_by_geography_groups
+    return azure_supported_locations_obj_by_geography_groups
+
+# get_azure_location_detail - Lists Azure location detail
+def get_azure_location_detail(azure_location_name):
+    azure_locations_list = utils.azure_cli_run_command('account list-locations')
+
+    for azure_location in azure_locations_list:
+        if azure_location["name"] == azure_location_name:
+            return azure_location
 
 # TODO: get_azure_supported_locations_sdk - Lists all supported locations for Azure in the current subscription via Azure SDK.
 def get_azure_supported_locations_sdk():
@@ -78,10 +90,10 @@ def get_azure_supported_locations_sdk():
 
     subscription_id = utils.get_subscription_id()
 
-    az_locations_iter = subscription_client.subscriptions.list_locations(subscription_id)    
+    azure_locations_iter = subscription_client.subscriptions.list_locations(subscription_id)    
     
-    az_locations_list = []
-    for location in az_locations_iter:
-        az_locations_list.append(location.name)
+    azure_locations_list = []
+    for location in azure_locations_iter:
+        azure_locations_list.append(location.name)
 
-    return az_locations_list
+    return azure_locations_list
