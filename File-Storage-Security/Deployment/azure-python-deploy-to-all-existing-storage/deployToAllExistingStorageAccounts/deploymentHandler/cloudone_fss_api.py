dc5284422699e215e0593f76a4d77b0aa6f7744d
George Davis <george_davis@trendmicro.com>
2022-07-19 23:22:09 -0400

    Cleanup, adding cloudone registration for scanner and storage stacks

--- File-Storage-Security/Deployment/azure-python-deploy-to-all-existing-storage/deployToAllExistingStorageAccounts/deploymentHandler/cloudone_fss_api.py
+++ File-Storage-Security/Deployment/azure-python-deploy-to-all-existing-storage/deployToAllExistingStorageAccounts/deploymentHandler/cloudone_fss_api.py
@@ -4,7 +4,7 @@ http = urllib3.PoolManager()
 
 import utils
 
-def filter_stacks_by_subscription_id(subscription_id, cloudone_fss_stacks_output):
+def filter_stacks_by_subscription_id(subscription_id, cloudone_fss_stacks_output):    
 
     temp_stacks_output_list = []
     for stack in cloudone_fss_stacks_output["stacks"]:             
@@ -19,9 +19,8 @@ def get_scanner_stacks():
     try:
         region = utils.get_cloudone_region()
         api_key = utils.get_cloudone_api_key()
-        subscription_id = utils.get_subscription_id()
         
-        if region and api_key and subscription_id:
+        if region and api_key:
             cloudone_fss_api_url = "https://filestorage.{}.cloudone.trendmicro.com/api".format(region)
 
             r = http.request(
@@ -44,9 +43,8 @@ def get_storage_stacks():
     try:    
         region = utils.get_cloudone_region()
         api_key = utils.get_cloudone_api_key()
-        subscription_id = utils.get_subscription_id()
 
-        if region and api_key and subscription_id:
+        if region and api_key:
             cloudone_fss_api_url = "https://filestorage.{}.cloudone.trendmicro.com/api".format(region)
 
             r = http.request(
@@ -99,6 +97,79 @@ def get_associated_storage_stacks_to_scanner_stack(scanner_stack_uuid):
                     "Api-Version": "v1",
                 },
 
+            )
+            if r.status == 200:
+                return json.loads(r.data)
+            else:
+                raise Exception("HTTP Request failure (code: " + str(r.status) + "). Check cloudone section in the config.json file or environment variables [\"CLOUDONE_API_KEY\", \"CLOUDONE_REGION\"] for valid input.")
+    except:
+        raise Exception("HTTP Request failure (code: " + str(r.status) + "). Check the logs for more information.")
+
+def register_scanner_stack_with_cloudone(resource_group_id, tenant_id):
+
+    try:    
+        region = utils.get_cloudone_region()
+        api_key = utils.get_cloudone_api_key()
+        
+        if region and api_key:
+            cloudone_fss_api_url = "https://filestorage.{}.cloudone.trendmicro.com/api".format(region)
+
+            request_data = {
+                'type': 'scanner',
+                'provider': 'azure',
+                'details': {
+                    'resourceGroupID': resource_group_id,
+                    'tenantID': tenant_id
+                }
+            }
+
+            # print(str(request_data))
+
+            r = http.request(
+                "POST",
+                cloudone_fss_api_url + "/stacks",
+                headers={
+                    "Authorization": "ApiKey " + api_key,
+                    "Api-Version": "v1",
+                },    
+                body=json.dumps(request_data)        
+            )
+            if r.status == 200:
+                return json.loads(r.data)["stackID"]
+            else:
+                raise Exception("HTTP Request failure (code: " + str(r.status) + "). Check cloudone section in the config.json file or environment variables [\"CLOUDONE_API_KEY\", \"CLOUDONE_REGION\"] for valid input.")
+    except:
+        raise Exception("HTTP Request failure (code: " + str(r.status) + "). Check the logs for more information.")
+
+def register_storage_stack_with_cloudone(cloudone_scanner_stack_id, resource_group_id, tenant_id):
+
+    try:    
+        region = utils.get_cloudone_region()
+        api_key = utils.get_cloudone_api_key()
+        
+        if region and api_key:
+            cloudone_fss_api_url = "https://filestorage.{}.cloudone.trendmicro.com/api".format(region)
+
+            request_data = {
+                'type': 'storage',
+                'provider': 'azure',
+                'scannerStack': cloudone_scanner_stack_id,
+                'details': {
+                    'resourceGroupID': resource_group_id,
+                    'tenantID': tenant_id
+                }
+            }
+
+            # print(str(request_data))
+
+            r = http.request(
+                "POST",
+                cloudone_fss_api_url + "/stacks",
+                headers={
+                    "Authorization": "ApiKey " + api_key,
+                    "Api-Version": "v1",
+                },    
+                body=json.dumps(request_data)        
             )
             if r.status == 200:
                 return json.loads(r.data)
