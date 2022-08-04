5e9b03ba4bb78f34915027bca31cecb77945c9a8
George Davis <george_davis@trendmicro.com>
2022-08-03 21:04:47 -0400

    refactor: Cleaning up

--- File-Storage-Security/Deployment/azure-python-deploy-to-all-existing-storage/deployToAllExistingStorageAccounts/deploymentHandler/geographies.py
+++ File-Storage-Security/Deployment/azure-python-deploy-to-all-existing-storage/deployToAllExistingStorageAccounts/deploymentHandler/geographies.py
@@ -1,4 +1,3 @@
-from operator import ge
 import locations
 
 def get_geographies_from_storage_accounts(azure_storage_account_list, azure_supported_locations_obj_by_geography_groups_dict):
@@ -9,7 +8,7 @@ def get_geographies_from_storage_accounts(azure_storage_account_list, azure_supp
 
         azure_geography_group = get_geography_group_from_location(storage_account["location"], azure_supported_locations_obj_by_geography_groups_dict)
 
-        print("\nStorage Account - " + str(azure_geography_group))
+        # logging.info("Storage Account - " + str(azure_geography_group))
 
         if azure_geography_group not in unique_scanner_stack_list:
             unique_scanner_stack_list.append(azure_geography_group)
@@ -30,17 +29,12 @@ def build_geographies_map_dict():
 
     azure_supported_locations_obj_by_geography_groups_dict = locations.get_azure_supported_locations()
 
-    # print(str(azure_supported_locations_obj_by_geography_groups_dict))
-
     for azure_geography_group in azure_supported_locations_obj_by_geography_groups_dict:
         if azure_geography_group not in geography_map_dict:
             geography_map_dict.update({azure_geography_group: []})
 
     # Remove any logical Azure Locations in Map Dictionary
     geography_map_dict.pop("logical")
-
-    for geography in geography_map_dict.keys():
-        print("\n\t\t\t" + geography)
 
     return geography_map_dict
 
