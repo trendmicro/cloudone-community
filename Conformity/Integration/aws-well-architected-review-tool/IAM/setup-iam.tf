425218b74ae3bdb3b3c21409ff5b722cb9d56cad
Felipe Costa <33869171+felipecosta09@users.noreply.github.com>
2022-08-05 15:08:33 +0000

    Make the role and policy more dynamic and improve the outputs.

--- Conformity/Integration/aws-well-architected-review-tool/IAM/setup-iam.tf
+++ Conformity/Integration/aws-well-architected-review-tool/IAM/setup-iam.tf
@@ -52,6 +52,7 @@ data "conformity_external_id" "all"{}
 
 # Create IAM policy
 resource "aws_iam_policy" "well-architected-tool-policy" {
+  name_prefix = "war-policy-"
   description = "CloudOne Well-Architected IAM policy"
   policy = jsonencode({
   "Version": "2012-10-17",
@@ -66,10 +67,14 @@ resource "aws_iam_policy" "well-architected-tool-policy" {
     }
   ]
 })
+    tags = {
+    Name = "well-architected-tool-policy"
+  }
 }
 
 # Create IAM role
 resource "aws_iam_role" "well-architected-tool-role" {
+  name_prefix = "war-role-"
   assume_role_policy = jsonencode({
   "Version": "2012-10-17",
   "Statement": [
@@ -100,6 +105,15 @@ resource "aws_iam_role_policy_attachment" "policy-attachment" {
 }
 
 # Output
-output "well-architected-toolarn" {
+output "well-architected-tool-role-name" {
+  value = aws_iam_role.well-architected-tool-role.name
+}
+output "well-architected-tool-role-arn" {
   value = aws_iam_role.well-architected-tool-role.arn
+}
+output "well-architected-tool-policy-name" {
+  value = aws_iam_policy.well-architected-tool-policy.name
+}
+output "well-architected-tool-policy-arn" {
+  value = aws_iam_policy.well-architected-tool-policy.arn
 }
