ee4f99f11f899f5e94633c56f4bfcdae33228bc4
George Davis <george_davis@trendmicro.com>
2022-08-09 20:24:16 -0400

    feat: Adding new functionality to list resources from resource_group, tagging resources, using glom instead of munch, using environment variables

--- File-Storage-Security/Deployment/azure-python-deploy-to-all-existing-storage/deployToAllExistingStorageAccounts/deploymentHandler/utils.py
+++ File-Storage-Security/Deployment/azure-python-deploy-to-all-existing-storage/deployToAllExistingStorageAccounts/deploymentHandler/utils.py
@@ -4,33 +4,66 @@ import tempfile
 import logging
 
 from azure.cli.core import get_default_cli
-# from munch import DefaultMunch 
+from azure.identity import ClientSecretCredential
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.storage import StorageManagementClient
+from azure.mgmt.storage.models import StorageAccountUpdateParameters
+from glom import glom
 
 import cloudone_fss_api
 import locations
 
 # compose_tags: Adds the FSSMonitored tag to the Storage Account(s) that are monitored by Trend Micro File Storage Security
-def compose_tags(existing_tags, FSS_MONITORED_TAG):
-    return {
-        **existing_tags,
-        **{
-            f'{FSS_MONITORED_TAG}': True
-        }
-    }
+def compose_tags(existing_tags, new_tags):
+    existing_tags.update(new_tags)
+    return existing_tags
+
+# tag_resource: Adds the FSSMonitored tag to the Storage Account(s) that are monitored by Trend Micro File Storage Security
+def tag_resource(subscription_id, resource_group_name, storage_account_name, resource_type, tags_dict):
+    if resource_type == "storage_account":
+        credentials = ClientSecretCredential(
+            client_id=os.environ['AZURE_CLIENT_ID'],
+            client_secret=os.environ['AZURE_CLIENT_SECRET'],
+            tenant_id=os.environ['AZURE_TENANT_ID']       
+        )
+        client = StorageManagementClient(credentials, subscription_id)
+        storage_account_tags = client.storage_accounts.get_properties( 
+            resource_group_name = resource_group_name,
+            account_name = storage_account_name
+        )
+        tags_dict = compose_tags(storage_account_tags.tags, tags_dict)
+        # print("\n\ntags_dict - " + str(tags_dict))
+        return client.storage_accounts.update(
+            resource_group_name = resource_group_name,
+            account_name = storage_account_name,
+            parameters = StorageAccountUpdateParameters(
+                tags = tags_dict
+            )
+        )
 
 # get_deployment_mode_from_env: Gets the deployment mode this script is executed with
 def get_deployment_mode_from_env(mode_key, DEPLOYMENT_MODES, DEFAULT_DEPLOYMENT_MODE):
 
     # Default mode is 'existing' storage accounts only
-    mode = os.environ.get(mode_key, 'existing').lower()
-    return mode if mode in DEPLOYMENT_MODES else DEFAULT_DEPLOYMENT_MODE
+    deployment_mode = str(get_config_from_file('deployment_mode'))
+    if deployment_mode:
+        return deployment_mode if deployment_mode in DEPLOYMENT_MODES else DEFAULT_DEPLOYMENT_MODE
+    if mode_key in os.environ.keys():
+        return os.environ.get(mode_key)  
+    logging.error("Missing Deployment Mode selection. Check \"" + mode_key + "\" env. variable or \"deployment_mode\" in the config.json file.")
+    raise Exception("Missing Deployment Mode selection. Check \"" + mode_key + "\" env. variable or \"deployment_mode\" in the config.json file.")
 
 # get_deployment_model_from_env: Gets the deployment model this script is executed with
 def get_deployment_model_from_env(model_key, DEPLOYMENT_MODELS, DEFAULT_DEPLOYMENT_MODEL):
 
     # Default model is 'geographies'
-    model = os.environ.get(model_key, 'geographies').lower()
-    return model if model in DEPLOYMENT_MODELS else DEFAULT_DEPLOYMENT_MODEL
+    deployment_model = str(get_config_from_file('deployment_model'))
+    if deployment_model:
+        return deployment_model if deployment_model in DEPLOYMENT_MODELS else DEFAULT_DEPLOYMENT_MODEL
+    if model_key in os.environ.keys():
+        return os.environ.get(model_key)  
+    logging.error("Missing Deployment Model selection. Check \"" + model_key + "\" env. variable or \"deployment_model\" in the config.json file.")
+    raise Exception("Missing Deployment Model selection. Check \"" + model_key + "\" env. variable or \"deployment_model\" in the config.json file.")
 
 # def get_blob_account_url(file_url):
 #     return '/'.join(file_url.split('/')[0:3])
@@ -91,53 +124,57 @@ def apply_exclusions(filename, azure_storage_account_list):
 #             yield from get_all_keys(value)
 
 def get_config_from_file(config_key):
+    try:
+        with open('config.json', 'r+') as f:
+            json_object = json.loads(f.read())
 
-    with open('config.json', 'r+') as f:
-        json_object = json.loads(f.read())
+        # Accessing Dict with Object notation for complex queries, using Glom
+        if "." in config_key:          
+            config_value = glom(json_object, config_key)
+            if config_value:
+                return config_value
 
-    # # Accessing Dict with Object notation for complex queries, using Munch
-    # if "." in config_key:
-    #     # d = AttrDict(json_object)
-    #     # print(dir(d))
-    #     # print(d.)
-    #     # return d
+        env_key = config_key.replace(".", "_").upper()        
 
-    #     d = DefaultMunch.fromDict(json_object)
-    #     print("Munch: " + str(d[config_key]))
-    #     return d[config_key]
+        if env_key in os.environ.keys():
+            env_value = os.environ.get(env_key)
+            if env_value:
+                return env_value
 
-    if json_object[config_key]:
-        return json_object[config_key]
-    return None
+        if json_object[config_key]:
+            return json_object[config_key]
+    except:
+        logging.error("ConfigError. Check environment variable \"" + env_key + "\" or \"" + config_key + "\" in the config.json file.")
+        raise Exception("ConfigError. Check environment variable \"" + env_key + "\" or \"" + config_key + "\" in the config.json file.")
 
-def get_cloudone_region():
-    cloudone_config = get_config_from_file('cloudone')
-    if cloudone_config and "region" in cloudone_config.keys():
-        if cloudone_config['region']:
-            return str(cloudone_config['region'])
-    if 'CLOUDONE_REGION' in os.environ.keys():
-        return os.environ.get('CLOUDONE_REGION')
-    logging.error("Missing Cloud One Region. Check \"CLOUDONE_REGION\" env. variable or \"cloudone.region\" in the config.json file.")
-    raise Exception("Missing Cloud One Region. Check \"CLOUDONE_REGION\" env. variable or \"cloudone.region\" in the config.json file.")
+# def get_cloudone_region():
+#     cloudone_config = get_config_from_file('cloudone')
+#     if cloudone_config and "region" in cloudone_config.keys():
+#         if cloudone_config['region']:
+#             return str(cloudone_config['region'])
+#     if 'CLOUDONE_REGION' in os.environ.keys():
+#         return os.environ.get('CLOUDONE_REGION')
+#     logging.error("Missing Cloud One Region. Check \"CLOUDONE_REGION\" env. variable or \"cloudone.region\" in the config.json file.")
+#     raise Exception("Missing Cloud One Region. Check \"CLOUDONE_REGION\" env. variable or \"cloudone.region\" in the config.json file.")
     
-def get_cloudone_api_key():
-    cloudone_config = get_config_from_file('cloudone')
-    if cloudone_config and "api_key" in cloudone_config.keys():
-        if cloudone_config['api_key']:
-            return str(cloudone_config['api_key'])
-    if 'CLOUDONE_API_KEY' in os.environ.keys():
-        return os.environ.get('CLOUDONE_API_KEY')
-    logging.error("Missing Cloud One API Key. Check \"CLOUDONE_API_KEY\" env. variable or \"cloudone.api_key\" in the config.json file.")
-    raise Exception("Missing Cloud One API Key. Check \"CLOUDONE_API_KEY\" env. variable or \"cloudone.api_key\" in the config.json file.")
+# def get_cloudone_api_key():
+#     cloudone_config = get_config_from_file('cloudone')
+#     if cloudone_config and "api_key" in cloudone_config.keys():
+#         if cloudone_config['api_key']:
+#             return str(cloudone_config['api_key'])
+#     if 'CLOUDONE_API_KEY' in os.environ.keys():
+#         return os.environ.get('CLOUDONE_API_KEY')
+#     logging.error("Missing Cloud One API Key. Check \"CLOUDONE_API_KEY\" env. variable or \"cloudone.api_key\" in the config.json file.")
+#     raise Exception("Missing Cloud One API Key. Check \"CLOUDONE_API_KEY\" env. variable or \"cloudone.api_key\" in the config.json file.")
 
-def get_cloudone_max_storage_to_scanner_count():
-    cloudone_config = get_config_from_file('cloudone')
-    if cloudone_config and "max_storage_stack_per_scanner_stack" in cloudone_config.keys():
-        if cloudone_config['max_storage_stack_per_scanner_stack']:
-            return str(cloudone_config['max_storage_stack_per_scanner_stack'])
-    if 'MAX_STORAGE_STACK_PER_SCANNER_STACK' in os.environ.keys():
-        return os.environ.get('MAX_STORAGE_STACK_PER_SCANNER_STACK')
-    return 50 # Recommended value for the number of Storage Stack(s) per Scanner Stack
+# def get_cloudone_max_storage_to_scanner_count():
+#     cloudone_config = get_config_from_file('cloudone')
+#     if cloudone_config and "max_storage_stack_per_scanner_stack" in cloudone_config.keys():
+#         if cloudone_config['max_storage_stack_per_scanner_stack']:
+#             return str(cloudone_config['max_storage_stack_per_scanner_stack'])
+#     if 'MAX_STORAGE_STACK_PER_SCANNER_STACK' in os.environ.keys():
+#         return os.environ.get('MAX_STORAGE_STACK_PER_SCANNER_STACK')
+#     return 50 # Recommended value for the number of Storage Stack(s) per Scanner Stack
 
 def get_subscription_id():
     azure_subscription_id = str(get_config_from_file('subscription_id'))
@@ -148,9 +185,23 @@ def get_subscription_id():
     logging.error("Missing Azure Subscription ID. Check \"AZURE_SUBSCRIPTION_ID\" env. variable or \"subscription_id\" in the config.json file.")
     raise Exception("Missing Azure Subscription ID. Check \"AZURE_SUBSCRIPTION_ID\" env. variable or \"subscription_id\" in the config.json file.")
 
+def get_deployment_tags():    
+    deployment_tags = get_config_from_file('deployment_tags')
+    if deployment_tags:
+        return deployment_tags
+    return None
+
 def get_subscription_id_from_resource_group_id(resource_group_id):
 
     return resource_group_id.split('/')[2]
+
+def get_resource_name_from_resource_id(resource_id):
+
+    return resource_id.split('/')[-1]
+
+def get_resource_group_name_from_resource_id(resource_id):
+
+    return resource_id.split('/')[4]
 
 def remove_storage_accounts_with_cloudone_storage_stacks(azure_storage_account_list):
 
@@ -172,6 +223,33 @@ def remove_storage_accounts_with_cloudone_storage_stacks(azure_storage_account_l
             azure_storage_account_list.remove(storage_account)        
 
     return azure_storage_account_list
+
+def remove_scanner_stacks_exceed_max_storage_account_count(scanner_stacks_map_by_geographies_dict, max_storage_to_scanner_count):
+
+    remove_scanner_stack_list = []
+
+    for scanner_stack_geography in scanner_stacks_map_by_geographies_dict:
+
+        for scanner_stack in scanner_stacks_map_by_geographies_dict[scanner_stack_geography]:
+
+            storage_account_count = len(cloudone_fss_api.get_associated_storage_stacks_to_scanner_stack(scanner_stack["stackID"])["stacks"])
+            # Update storageStackCount value to the scanner_stack
+            scanner_stack.update({"storageStackCount": storage_account_count})
+
+            if storage_account_count > max_storage_to_scanner_count:                
+                remove_scanner_stack_list.append(scanner_stack["stackID"])            
+    
+    for stack_id in remove_scanner_stack_list:
+
+        for scanner_stack_geography in scanner_stacks_map_by_geographies_dict:
+
+            for scanner_stack in scanner_stacks_map_by_geographies_dict[scanner_stack_geography]:
+                
+                if stack_id == scanner_stack["stackID"]:
+
+                    scanner_stacks_map_by_geographies_dict[scanner_stack_geography].remove(scanner_stack)
+
+    return scanner_stacks_map_by_geographies_dict
 
 # function to return dict key for any value
 def get_dict_key(value_dict, val):
@@ -196,7 +274,7 @@ def trim_location_name(azure_location_name):
                 string_output = str(item[0])
             else:
                 string_output = string_output + str(item[0])
-        string_output = string_output + str(temp_list[-1:][0][:3])
+        string_output = string_output + str(temp_list[-1][:3])
         return string_output.lower()
     return azure_location_name
 
@@ -209,8 +287,17 @@ def trim_spaces(string_value):
                 string_output = str(item[0])
             else:
                 string_output = string_output + str(item[0])
-        string_output = string_output + str(temp_list[-1:][0])
+        string_output = string_output + str(temp_list[-1])
         return string_output.lower()
     elif len(string_value) > 6:
         return string_value[:3].lower()
     return string_value.lower()
+
+def list_resources_in_resource_group(subscription_id, resource_group_name):
+    credentials = ClientSecretCredential(
+        client_id=os.environ['AZURE_CLIENT_ID'],
+        client_secret=os.environ['AZURE_CLIENT_SECRET'],
+        tenant_id=os.environ['AZURE_TENANT_ID']       
+    )
+    client = ResourceManagementClient(credentials, subscription_id)
+    return client.resources.list_by_resource_group(resource_group_name)
