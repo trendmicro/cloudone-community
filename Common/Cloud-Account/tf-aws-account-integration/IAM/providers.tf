a672e877c493ffb64797731e396a052f7e0d41fa
Felipe Costa <33869171+felipecosta09@users.noreply.github.com>
2022-08-10 11:14:58 -0500

    Remove unnecessary configs on providers

--- Common/Cloud-Account/tf-aws-account-integration/IAM/providers.tf
+++ Common/Cloud-Account/tf-aws-account-integration/IAM/providers.tf
@@ -5,14 +5,6 @@
        source  = "hashicorp/aws"
        version = "~> 4.22.0"
      }
-     restapi = {
-       source = "Mastercard/restapi"
-       version = "1.17.0"
-     }
-     time = {
-       source = "hashicorp/time"
-       version = "0.7.2"
-     }
    }
  }
 
@@ -20,16 +12,3 @@
  provider "aws" {
    region = var.region
  }
-
- provider "restapi" {
-   alias                = "restapi_headers"
-   uri                  = "https://cloudaccounts.${var.c1-region}.cloudone.trendmicro.com"
-   debug                = true
-   write_returns_object = true
-
-   headers = {
-     Accept = "application/json"
-     api-version = "v1"
-     Authorization = "ApiKey ${var.c1-api-key}"
-   }
- } 
