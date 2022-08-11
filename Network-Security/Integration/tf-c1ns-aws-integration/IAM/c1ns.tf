e8ab2789c9c8342b93a622289f677ea773e9e6f7
Felipe Costa <33869171+felipecosta09@users.noreply.github.com>
2022-08-11 09:35:03 -0500

    fix folder structure

--- Network-Security/Integration/tf-c1ns-aws-integration/IAM/c1ns.tf
+++
@@ -1,7 +0,0 @@
-# Add to Network Security
-resource "restapi_object" "c1ns-api-call" {
-  depends_on = [time_sleep.wait_10_seconds]
-  provider = restapi.restapi_headers
-  path = "/api/awsconnectors"
-  data = "{ \"accountName\": \"${var.aws-account-alias}\", \"crossAccountRole\": \"${aws_iam_role.network-security-role.arn}\", \"externalId\": \"${var.externalid}\" }"
-}
