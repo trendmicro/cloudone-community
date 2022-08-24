d277183b39ccb9e91d0bf7a46b1b448b1e0ef3c6
Justin Perkins <justin_perkins@trendmicro.com>
2022-08-23 22:55:29 -0500

    update: remove regional limitations

--- File-Storage-Security/Deployment/Post-Scan-Action-Automations/aws-slack-notification-automation/README.md
+++ File-Storage-Security/Deployment/Post-Scan-Action-Automations/aws-slack-notification-automation/README.md
@@ -4,16 +4,16 @@ This script will subscribe a deployed Slack plugin to all FSS storage stacks. Af
 **Before you deploy**
 
    * Install supporting tools
-        - Install adn configure the AWS command line interface (CLI). 
+        - Install and configure the AWS command line interface (CLI). 
         See [Installing the AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html) for details.
 
    * If not already present, [deploy the Slack Plugin for File Storage Security](https://github.com/trendmicro/cloudone-filestorage-plugins/tree/master/post-scan-actions/aws-python-slack-notification).
   
   * Obtain the following parameters.
-      - API Key - Generate a [Cloud One API Key](https://cloudone.trendmicro.com/docs/account-and-user-management/c1-api-key/)
-      - Cloud One Account [Region](https://cloudone.trendmicro.com/docs/identity-and-account-management/c1-regions/) - example: ```us-1```.
-      - Name of the Slack Lambda Function.
-      - ARN of the Slack Lambda Function.
+      - **API Key** - Generate a [Cloud One API Key](https://cloudone.trendmicro.com/docs/account-and-user-management/c1-api-key/)
+      - **Cloud One Account** [**Region**](https://cloudone.trendmicro.com/docs/identity-and-account-management/c1-regions/) - example: ```us-1```.
+      - **Name** of the Slack Lambda Function.
+      - **ARN** of the Slack Lambda Function.
 
 <hr>
 
@@ -27,6 +27,6 @@ This script will subscribe a deployed Slack plugin to all FSS storage stacks. Af
 **2. Run Script**
    - Open terminal/cmd:
    ```
-      .\auto-slack.py --apikey <apikey here> --c1region <c1 account region> --functionname <name of slack function --functionarn <slack function arn>
+      .\auto-slack.py --apikey <apikey here> --c1region <c1 account region> --functionname <name of slack function --functionarn <slack function arn> --awsprofile <> --awsregion <aws region to operate in>
    ```  
 
--- File-Storage-Security/Deployment/Post-Scan-Action-Automations/aws-slack-notification-automation/python-aws-automate-slack-subscription.py
+++ File-Storage-Security/Deployment/Post-Scan-Action-Automations/aws-slack-notification-automation/python-aws-automate-slack-subscription.py
@@ -1,23 +1,32 @@
 import boto3
+import botocore
+import random
+import string
 import urllib3
 import json
 import argparse
 
 http = urllib3.PoolManager()
 
-#variables needed
-parser = argparse.ArgumentParser(description='Deploy to All Buckets')
+#arguments require
+parser = argparse.ArgumentParser(description='Subscribe Slack Plugin to SNS')
 parser.add_argument("--apikey", required=True, type=str, help="Cloud One API Key")
 parser.add_argument("--c1region", required=True, type=str, help="Cloud One Account Region")
 parser.add_argument("--functionname", required=True, type=str, help="Name of Slack Lambda Function")
 parser.add_argument("--functionarn", required=True, type=str, help="ARN of Slack Lambda Function")
+parser.add_argument("--awsprofile", required=True, type=str, help="AWS Profile to use for credentials")
+parser.add_argument("--pluginregion", required=True, type=str, help="AWS Region Slack plugin is deployed")
 args = parser.parse_args()
 
 fss_api = args.apikey
 c1_region = args.c1region
 fx_name = args.functionname
 fx_arn = args.functionarn
+aws_profile = args.awsprofile
+plugin_region = args.pluginregion
+#fss base api call url
 call_url = "https://filestorage."+c1_region+".cloudone.trendmicro.com/api/"
+
 
 # obtain C1 FSS storage stacks
 def fss_list_stack():
@@ -30,37 +39,53 @@ def fss_list_stack():
         },
         
     )
-    stack_names = []
+    stacks_info = []
     response = json.loads(stacks.data.decode("utf-8"))
     for name in response['stacks']:
-        stack_names.append(name['name'])
-    describe_fss_storage(stack_names)
+        stacks_info.append([name['name'], name['details']['region']])
+    
+    describe_fss_storage(stacks_info)
 
 # describe cft stacks to obtain each storage stack sns arn.
-def describe_fss_storage(stack_names):
+def describe_fss_storage(stacks_info):
     
-    cft_client = boto3.client('cloudformation')
-    arnArray = []
-    for stack in stack_names:
-        response = cft_client.describe_stacks(StackName=stack)
-        outputs = response["Stacks"][0]["Outputs"]
+    for stack in stacks_info:
+        # set aws profile sessions
+        session = boto3.Session(profile_name=aws_profile, region_name=str(stack[1]))
+        cft_client = session.client('cloudformation')
+        try:
+            response = cft_client.describe_stacks(StackName=str(stack[0]))
+            outputs = response["Stacks"][0]["Outputs"]
 
-        for output in outputs:
-            keyName = output["OutputKey"]
-            if keyName == "ScanResultTopicARN":
-                arnArray.append(output["OutputValue"])
-    subscribe_sns(arnArray)    
+            for output in outputs:
+                keyName = output["OutputKey"]
+                if keyName == "ScanResultTopicARN":
+                    topic_arn = output["OutputValue"]
+                    subscribe_sns(topic_arn, session)
+        # Skip deploy FSS stacks that do not exist in current AWS account
+        except botocore.exceptions.ClientError as err:
+            print("Skipping " + str(stack[0]) + ", does not exist in this AWS Account")
 
 # subscribe lambda to sns topics to receive events
-def subscribe_sns(arnArray):
-    id = 1
-    for topic in arnArray:
-        print("Subscribing Lambda to: " + topic)
-        client = boto3.client('lambda')
-        client.add_permission(Action="lambda:InvokeFunction", StatementId=("automate-"+ str(id)),FunctionName=fx_name, Principal='sns.amazonaws.com', SourceArn=topic)
-        client = boto3.client('sns')
-        client.subscribe(TopicArn=topic,Protocol='lambda',Endpoint=fx_arn)
-        id = id + 1
+def subscribe_sns(topic_arn, session):
 
-
+    #for topic in arnArray:
+    try:    
+        client_lambda = session.client('lambda')
+        client_lambda.add_permission(Action="lambda:InvokeFunction", StatementId=(''.join(random.choices(string.ascii_lowercase, k=5))),FunctionName=fx_name, Principal='sns.amazonaws.com', SourceArn=topic_arn)
+        client_sns = session.client('sns')
+        client_sns.subscribe(TopicArn=topic_arn,Protocol='lambda',Endpoint=fx_arn)
+        print("Subscribing Lambda to: " + topic_arn)
+        
+    except botocore.exceptions.ClientError as error:
+        # handle invalid passed params
+        if error.response['Error']['Code'] == 'InvalidParameter':
+            print(error)
+        # handle subscription when topic lives in another AWS Region
+        elif error.response['Error']['Code'] == 'ResourceNotFoundException':
+            session = boto3.Session(profile_name=aws_profile, region_name=plugin_region)
+            client_lambda = session.client('lambda')
+            client_lambda.add_permission(Action="lambda:InvokeFunction", StatementId=(''.join(random.choices(string.ascii_lowercase, k=5))),FunctionName=fx_name, Principal='sns.amazonaws.com', SourceArn=topic_arn)
+            print("Subscribing Lambda to: " + topic_arn)
+            
 fss_list_stack()
