6b64efdfe6884b9e1fe15e8716d2625448e19d9b
Felipe Costa <33869171+felipecosta09@users.noreply.github.com>
2022-11-18 13:35:10 -0600

    add Conformity GCP deployment on terraform

---
+++ Conformity/Cloud-Account/gcp-tf-cloud-account-connector/IAM/variables.tf
@@ -0,0 +1,10 @@
+
+variable "region" {
+  description = "The region to deploy to"
+  default     = "us-central1"
+}
+
+variable "project_id" {
+  description = "The project ID to deploy to"
+  default = ""
+}
